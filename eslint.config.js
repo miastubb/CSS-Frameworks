29c89afc3644336cbad516aabd93827df23340ea
Mia  Stubb-Olsen <miastubb@gmail.com>
2026-06-01 18:41:08 +0200

    initialize css frameworks project structure

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
