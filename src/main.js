928b5623016a4fe2e6f4672c452f7cb7e87be27f
Mia  Stubb-Olsen <miastubb@gmail.com>
2026-06-06 00:18:10 +0200

    build profile page and add profile image

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import "./style.css";
 import { createHeader } from "./components/header.js";
+import miaAndPippin from "./pics/mia-and-pippin.jpg";
 
 const headerContainer = document.querySelector("#header");
 
 if (headerContainer) {
   headerContainer.innerHTML = createHeader();
+}
+
+const profileImage = document.querySelector("#profile-image");
+
+if (profileImage) {
+  profileImage.src = miaAndPippin;
 }
